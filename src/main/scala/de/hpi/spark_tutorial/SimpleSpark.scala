e34b215a54e15be131568c01515316f565e4c8eb
alpreu <11444089+alpreu@users.noreply.github.com>
2018-12-15 23:32:48 +0100

    adding CLI to program, not fully functional atm

--- src/main/scala/de/hpi/spark_tutorial/SimpleSpark.scala
+++ src/main/scala/de/hpi/spark_tutorial/SimpleSpark.scala
@@ -2,21 +2,45 @@ package de.hpi.spark_tutorial
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
+import org.apache.commons.cli._
 
 
 object SimpleSpark extends App {
 
   override def main(args: Array[String]): Unit = {
+    //config values
+    var cores = 4
+    var path = "./TPCH"
 
     // Turn off logging
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
 
+    // CLI parsing
+    val options = new Options
+    val pathOpt = Option.builder().longOpt("path").hasArg().desc("path to the TPCH files").build()
+    val coresOpt = Option.builder().longOpt("cores").hasArg().desc("number of worker cores").build()
+    options.addOption(pathOpt)
+    options.addOption(coresOpt)
+
+    val parser = new DefaultParser
+    try {
+      val cmd = parser.parse(options, args)
+      path =  cmd.getOptionValue("path", "./TPCH")
+      cores = cmd.getOptionValue("cores", "4").toInt
+    } catch {
+      case e : Exception => {
+        println(e, e.getMessage)
+        return
+      }
+    }
+
+
     // Create a SparkSession to work with Spark
     val sparkBuilder = SparkSession
       .builder()
       .appName("SparkTutorial")
-      .master("local[4]") // local, with 4 worker cores
+      .master(s"local[$cores]") // local, with $cores worker cores
     val spark = sparkBuilder.getOrCreate()
 
     // Set the default number of shuffle partitions (default is 200, which is too high for local deployment)
