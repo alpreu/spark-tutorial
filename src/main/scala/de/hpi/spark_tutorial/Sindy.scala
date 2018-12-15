54fc6257815c6687a7e48a83ed9b5c03c120352a
alpreu <11444089+alpreu@users.noreply.github.com>
2018-12-15 22:38:12 +0100

    implementing complete ind pipeline

--- src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -37,20 +37,28 @@ object Sindy {
 //    tables.foreach(df => df.show())
 //    tables.foreach(df => df.printSchema())
 
-
-//    val columns = tables.flatMap(df => df.columns.map(col => df.select(col)))
-    val columns = tables.flatMap(df => df.columns.map(col => df.select(col).distinct())) // not sure atm if unique values give us a benefit
+    val columns = tables.flatMap(df => df.columns.map(col => df.select(col).distinct())) // TODO: not sure atm if unique values give us a benefit
 //    columns.foreach(df => df.show())
 
-
-    val valueColumnPairs = columns.map(col => (
-      col.map(row => (row.get(0).toString, row.schema.fieldNames.head))
-    )).reduce((c1, c2) => c1.union(c2)).rdd
+    val valueColumnPairs = columns.map(col => col
+      .map(row => (row.get(0).toString, row.schema.fieldNames.head)))
+      .reduce((c1, c2) => c1.union(c2)).rdd
 //    valueColumnPairs.foreach(c => println(c))
 
-    val columnsByValue = valueColumnPairs.groupByKey()//.mapValues(List(_))
-    columnsByValue.foreach(e => println(e))
+    val columnsByValue = valueColumnPairs.groupByKey()
+//    columnsByValue.foreach(e => println(e))
 
+    val attributeSets = columnsByValue.map(_._2.toSet).distinct() // TODO: not sure atm if unique values give us a benefit
+//    attributeSets.foreach(e => println(e))
+
+    val inclusionLists = attributeSets.flatMap(set => set.map(col => (col, set.filter(otherCol => !otherCol.equals(col)))))
+//    inclusionLists.foreach(e => println(e))
+
+    val aggregates = inclusionLists.reduceByKey((refs1, refs2) => refs1.intersect(refs2)).filter(_._2.nonEmpty)
+//    aggregates.foreach(e => println(e))
+
+    val output = aggregates.collect().map(ind => ind._1.concat(" < ").concat(ind._2.mkString(", "))).sorted
+    output.foreach(e => println(e))
 
   }
 }
