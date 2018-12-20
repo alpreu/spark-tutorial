76cc4610fd0cf7b5c6a2f11c96a19fe5197944fa
alpreu <11444089+alpreu@users.noreply.github.com>
2018-12-20 14:38:48 +0100

    small cleanup

--- src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -7,7 +7,7 @@ object Sindy {
   
   def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
 
-    println(inputs)
+    println("Discovering INDs in ".concat(inputs.mkString(", ")))
 
     import spark.implicits._
 
@@ -18,28 +18,19 @@ object Sindy {
       .csv(path)
     )
 
-//    tables.foreach(df => df.show())
-//    tables.foreach(df => df.printSchema())
-
     val columns = tables.flatMap(df => df.columns.map(col => df.select(col).distinct())) // TODO: not sure atm if unique values give us a benefit
-//    columns.foreach(df => df.show())
 
     val valueColumnPairs = columns.map(col => col
       .map(row => (row.get(0).toString, row.schema.fieldNames.head)))
       .reduce((c1, c2) => c1.union(c2)).rdd
-//    valueColumnPairs.foreach(c => println(c))
 
     val columnsByValue = valueColumnPairs.groupByKey()
-//    columnsByValue.foreach(e => println(e))
 
     val attributeSets = columnsByValue.map(_._2.toSet).distinct() // TODO: not sure atm if unique values give us a benefit
-//    attributeSets.foreach(e => println(e))
 
     val inclusionLists = attributeSets.flatMap(set => set.map(col => (col, set.filter(otherCol => !otherCol.equals(col)))))
-//    inclusionLists.foreach(e => println(e))
 
     val aggregates = inclusionLists.reduceByKey((refs1, refs2) => refs1.intersect(refs2)).filter(_._2.nonEmpty)
-//    aggregates.foreach(e => println(e))
 
     val output = aggregates.collect().map(ind => ind._1.concat(" < ").concat(ind._2.mkString(", "))).sorted
     output.foreach(e => println(e))
